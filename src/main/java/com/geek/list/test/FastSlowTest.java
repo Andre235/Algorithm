78121b603a33fb0e474baf8305060b5ee090b451
zhaojingchao <2356430623@qq.com>
2021-12-28 14:10:54 +0800

    链表：快慢指针实现获取中间值

---
+++ src/main/java/com/geek/list/test/FastSlowTest.java
@@ -0,0 +1,62 @@
+package com.geek.list.test;
+
+import lombok.Data;
+import lombok.Getter;
+
+/**
+ * @author: lucas.zhao@kuhantech.com
+ * @date: 2021/12/27 17:56
+ * @description:
+ */
+public class FastSlowTest {
+    public static void main(String[] args) {
+
+        Node<String> first = new Node<>("1", null);
+        Node<String> second = new Node<>("2", null);
+        Node<String> third = new Node<>("3", null);
+        Node<String> fourth = new Node<>("4", null);
+        Node<String> fifth = new Node<>("5", null);
+        Node<String> six = new Node<>("6", null);
+        Node<String> seven = new Node<>("7", null);
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = six;
+        six.next = seven;
+
+        String value = middleValue(first);
+        System.out.println("中间值：" + value);
+    }
+
+    private static <T> T middleValue(Node<T> node) {
+        // 1.定义两个指针
+        Node<T> fastNode = node;
+        Node<T> slowNode = node;
+
+        // 2.使用两个指针遍历链表，当快指针指向的节点没有下一个节点时，此时遍历结束
+        // 这时慢指针指向的节点就是中间值
+        while (fastNode.next != null) {
+            // 变化快慢指针的值
+            fastNode = fastNode.next.next;
+            if (fastNode == null) {
+                throw new NullPointerException("当前链表长度为偶数个，不能取中间值");
+            }
+            slowNode = slowNode.next;
+        }
+        return slowNode.data;
+    }
+
+
+    @Data
+    public static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
